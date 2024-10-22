f7525988de42048978f6676b910fd803be4d3a1e
Euan <ew154461@renishaw.com>
2024-10-22 16:22:02 +0100

    finished project, supposedly with 15 tests passed and 1 failed, but I can't understand why

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,22 @@
-// import the Media class:
+// Import the Media class
+const Media = require('./Media');
 
-// create your Book class:
+class Book extends Media {
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static highestRating(books) {
+        return books.reduce((highest, book) => book.rating > highest.rating ? book : highest);
+    }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,21 @@
-// import the Media class:
+const Media = require('./Media');
 
-// create your Movie class:
+class Movie extends Media {
+    constructor(title, year, genre, director, duration, rating) {
+        super(title, year, genre); 
+        this.director = director;
+        this.duration = duration; 
+        this.rating = rating;
+    }
 
-// don't change below
+    summary() {
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static longestMovie(movies) {
+        return movies.reduce((longest, movie) => movie.duration > longest.duration ? movie : longest);
+    }
+}
+
+// Export the Movie class
 module.exports = Movie;
